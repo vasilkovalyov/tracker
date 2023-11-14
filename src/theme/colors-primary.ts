3b6b3526b1ede4f1452d820c270a071370c06c18
vasilkovalyov <vasilkovalyov@gmail.com>
2023-11-14 08:24:53 +0200

    setup mui theme

---
+++ src/theme/colors-primary.ts
@@ -0,0 +1,12 @@
+const colors = {
+  grey: '#A3A3A3',
+  grey_light: '#FAFAFA',
+  grey_dark: '#615C5C',
+  black: '#000000',
+  green: '#2BB272',
+  white: '#ffffff',
+  primary: '#FF643B',
+  secondary: '#8234F8',
+}
+
+export default colors
---
+++ src/theme/primary.ts
@@ -0,0 +1,145 @@
+import { createTheme } from '@mui/material/styles'
+
+import colors from './colors-primary'
+
+const theme = createTheme({
+  typography: {
+    allVariants: {
+      fontFamily: 'Roboto, sans-serif',
+    },
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 576,
+      md: 768,
+      lg: 1024,
+      xl: 1440,
+    },
+  },
+  palette: {
+    primary: {
+      main: colors.primary,
+      contrastText: colors.white,
+    },
+    secondary: {
+      main: colors.secondary,
+      contrastText: colors.white,
+    },
+    success: {
+      main: colors.green,
+      contrastText: colors.white,
+    },
+    info: {
+      main: colors.grey_dark,
+      contrastText: colors.white,
+      light: colors.grey,
+    },
+  },
+  components: {
+    MuiSelect: {
+      styleOverrides: {
+        select: {
+          width: '100%',
+          backgroundColor: colors.primary,
+          marginTop: 0,
+          height: 20,
+          paddingTop: 8,
+          paddingBottom: 8,
+          paddingLeft: 20,
+          paddingRight: 20,
+          borderRadius: 20,
+          overflow: 'hidden',
+          borderWidth: 2,
+          borderStyle: 'solid',
+          borderColor: 'transparent',
+        },
+      },
+    },
+  },
+})
+
+// **** TYPOGRAPHY ****
+const fontSizeH1 = {
+  color: colors.black,
+  fontSize: 24,
+  fontWeight: 500,
+  marginBottom: 16,
+  border: 'none',
+  borderBlockColor: 'transparent',
+  fontFamily: 'Roboto, sans-serif',
+}
+
+theme.typography.h1 = fontSizeH1
+theme.typography.h2 = fontSizeH1
+
+theme.typography.h3 = {
+  color: colors.black,
+  fontSize: 20,
+  marginBottom: 16,
+  border: 0,
+  fontWeight: 500,
+  fontFamily: 'Roboto, sans-serif',
+}
+
+theme.typography.h4 = {
+  color: colors.black,
+  fontSize: 18,
+  marginBottom: 16,
+  border: 0,
+  fontWeight: 500,
+  fontFamily: 'Roboto, sans-serif',
+}
+
+theme.typography.h5 = {
+  color: colors.black,
+  fontSize: 16,
+  marginBottom: 16,
+  fontWeight: 500,
+  fontFamily: 'Roboto, sans-serif',
+}
+
+theme.typography.h6 = {
+  color: colors.black,
+  fontSize: 14,
+  marginBottom: 16,
+  border: 0,
+  fontWeight: 400,
+  fontFamily: 'Roboto, sans-serif',
+}
+
+theme.typography.body1 = {
+  color: colors.grey_dark,
+  fontSize: 16,
+  marginBottom: 16,
+  border: 0,
+  fontFamily: 'Roboto, sans-serif',
+}
+
+theme.typography.body2 = {
+  color: colors.grey_dark,
+  fontSize: 16,
+  marginBottom: 16,
+  border: 0,
+  fontFamily: 'Roboto, sans-serif',
+}
+
+theme.typography.subtitle1 = {
+  fontSize: 12,
+  color: colors.grey,
+  fontWeight: 300,
+  marginBottom: 16,
+  fontFamily: 'Roboto, sans-serif',
+}
+
+theme.typography.subtitle2 = {
+  fontSize: 12,
+  color: colors.grey,
+  fontWeight: 300,
+  marginBottom: 16,
+  fontFamily: 'Roboto, sans-serif',
+}
+
+// ************
+
+export default theme
